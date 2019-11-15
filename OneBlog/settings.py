908cf4fc3246794335dfb5fbaf45bc4d30e15488
pavan kumar <kpavankumar623@hotmail.com>
2019-11-15 19:25:04 +0530

    allowing hostname

--- OneBlog/settings.py
+++ OneBlog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'#deploy
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1','blog-sharing.herokuapp.com']
 
 # Application definition
 
