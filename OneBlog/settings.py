2577b42f37888b160c2a6b37594cfaa7f69c0741
pavankumar <kpavankumar623@hotmail.com>
2019-11-15 19:08:37 +0530

    stsatic file loading

--- OneBlog/settings.py
+++ OneBlog/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 import django_heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
